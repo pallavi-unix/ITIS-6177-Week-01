b36c97c64bd245bb45b47bb9dc87ca233ebe42cf
Pallavi Deshmukh <71973959+pallavi-unix@users.noreply.github.com>
2023-08-30 14:06:10 -0400

    Modified the file

--- HelloWorld.js
+++ HelloWorld.js
@@ -2,6 +2,10 @@ console.log("Hello World!");
 
 const http = require('http');
 
+const express = require('express');
+const app = express();
+const port = 8000;
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('This is a Node.js server.');
@@ -9,4 +13,12 @@ const server = http.createServer((req, res) => {
 
 server.listen(3000, '127.0.0.1', () => {
   console.log('Server is listening on http://127.0.0.1:3000');
+});
+
+app.get('/', (req, res) => {
+    res.send('This is an Express server.');
+});
+  
+app.listen(port, () => {
+    console.log(`Server is listening on http://localhost:${port}`);
 });
