bd33621c247d018c4c230ffb364516bf65087bc7
Pallavi Deshmukh <pallavideshmukh32897@gmail.com>
2023-08-30 13:30:36 -0400

    Modified the file by creating a server

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,12 @@
 console.log("Hello World!");
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('This is a Node.js server.');
+});
+
+server.listen(3000, '127.0.0.1', () => {
+  console.log('Server is listening on http://127.0.0.1:3000');
+});
